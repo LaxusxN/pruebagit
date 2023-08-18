14155c1e67e89bcec07c333206c34ed61cd25f05
Jackelin <jklnuag08@gmail.com>
2023-08-18 16:29:01 -0500

    modificacion en el borde

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,42 +2,43 @@ import React from 'react';
 
 export default function Home() {
   return (
-    <body className='flex'>
-      <h1 className='titulos'>LOGIN DE LEVEL STUDIO</h1>
-      <div className="login_box">
-      <form>
-          <label htmlFor="usuario">Usuario</label>
-          <input
-            id="usuario"
-            className="login-input"
-            type="text"
-            placeholder="Usuario"
-            required
-          />
-          <label htmlFor="contraseña">Contraseña</label>
-          <input
-            id="contraseña"
-            className="login-input"
-            type="password"
-            placeholder="Contraseña"
-            required
-          />
-          <div className='flex'>
-            <label>Genero </label>
-              <select>
-                <option value ="hombre"> hombre</option>
-                <option value ="mujer">mujer</option>
-              </select>
+    <body>
+      <div className='flex'>
+          <h1 className='titulo'>LOGIN DE LEVEL STUDIO</h1>
+          <div className="login_box">
+            <form>
+              <label htmlFor="usuario">Usuario</label>
+              <input
+                id="usuario"
+                className="login-input"
+                type="text"
+                placeholder="Usuario"
+                required
+              />
+              <label htmlFor="contraseña">Contraseña</label>
+              <input
+                id="contraseña"
+                className="login-input"
+                type="password"
+                placeholder="Contraseña"
+                required
+              />
+              <div className='flex'>
+                <label>Genero</label>
+                <select>
+                  <option value="hombre">Hombre</option>
+                  <option value="mujer">Mujer</option>
+                </select>
+              </div>
+              <div className='flex'>
+                <button className="login-button" type="submit">
+                 olvide mi contraseña
+                </button>
+                <a href='#'>Iniciar sesion</a>
+              </div>
+            </form>
           </div>
-          <div className='flex'>
-            <button className="login-button" type="submit">
-              Iniciar sesión
-            </button>
-          </div>
-          <a href='#'> forget password </a>
-      </form>
       </div>
-     
     </body>
   );
 }
