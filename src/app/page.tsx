1482d35634a07482656b289560362528c7fa19b9
Jackelin <jklnuag08@gmail.com>
2023-08-17 22:17:09 -0500

    mi login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,43 @@
-
+import React from 'react';
 
 export default function Home() {
   return (
-  <main>
-  <h1>Hola mundo, este un ejercicio de prueba</h1>
-  <h2>hola  soy  Jackelin ,   soy  estudiante  de villarelax</h2>
-  </main>
-  )
+    <body className='flex'>
+      <h1 className='titulos'>LOGIN DE LEVEL STUDIO</h1>
+      <div className="login_box">
+      <form>
+          <label htmlFor="usuario">Usuario</label>
+          <input
+            id="usuario"
+            className="login-input"
+            type="text"
+            placeholder="Usuario"
+            required
+          />
+          <label htmlFor="contraseña">Contraseña</label>
+          <input
+            id="contraseña"
+            className="login-input"
+            type="password"
+            placeholder="Contraseña"
+            required
+          />
+          <div className='flex'>
+            <label>Genero </label>
+              <select>
+                <option value ="hombre"> hombre</option>
+                <option value ="mujer">mujer</option>
+              </select>
+          </div>
+          <div className='flex'>
+            <button className="login-button" type="submit">
+              Iniciar sesión
+            </button>
+          </div>
+          <a href='#'> forget password </a>
+      </form>
+      </div>
+     
+    </body>
+  );
 }
